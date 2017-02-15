005f9a29f53bc366dd222b871b12237fcd091b75
EVA-03-PC\EVA-03 <thanhlt2@evolableasia.vn>
2017-02-15 17:07:55 +0700

    Update config with database connection

--- application/controllers/demo.php
+++ application/controllers/demo.php
@@ -1,16 +1,38 @@
 <?php 
 class Demo extends CI_Controller{
-
-    public function __constructor(){
-        parent::__constructor();
-        
-    }
-    public function index() {
+    function __construct(){
+        parent::__construct();
+        $this->load->helper("url");
         $this->load->library("session");
+        $this->session->set_flashdata("ten", "value001");
+    }
+    public function index($mess = "") {
+        echo $mess;
         $this->session->set_userdata("ten", "thanh_betonamujin");
         echo ($this->session->userdata("ten"));
-	
-        // $this->session->sess_destroy();
+        $data=array(
+            "username" => "Kaito",
+            "email" => "codephp2013@gmail.com",
+            "website" => "freetuts.net",
+            "gender" => "Male",
+        );
+        $this->session->set_userdata($data);
+        echo ("<br>".$this->session->userdata("username"));
+        $arrData = $this->session->all_userdata();
+        echo "<pre>";
+        print_r($arrData);
+        echo "</pre>";
+        $this->session->sess_destroy();
+        echo "Huy session thanh cong";
+        redirect(base_url(), "demo/index2");
+    }
+
+    public function index2(){
+        $user=$this->session->userdata("username");
+        $email=$this->session->userdata("email");
+        $website=$this->session->userdata("website");
+        $gender=$this->session->userdata("gender");
+        echo "Username : .$user, Email: $email, Gender: $gender";
     }
 }
 ?>
--- application/controllers/news.php
+++ application/controllers/news.php
@@ -2,7 +2,9 @@
 class News extends CI_Controller{
     function new_list()
     {
+        // load model
         $this->load->model('news_model');
+        // Goi function trong model
         $news_list = $this->news_model->getList();
     }
 }
